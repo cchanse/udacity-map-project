55ab297d8ff8555bb0113e6b9e623a1525f53d5a
Catherine Chanse <cchanse@frbsf.org>
2017-01-18 15:36:13 -0800

    fix: clean up comments and styles

---
+++ README.md
@@ -0,0 +1,14 @@
+# Neighborhood Map Project
+
+## About
+This is a single-page application featuring a map of different East Bay Agency for Children locations. Map markers identify different EBAC locations and will display address and services provided at the location when they are clicked on. The NYTimes API is used to pull a recent article that references that location, and is shown in the marker infowindow.
+
+A search input field allows user to filter by name, location, or service offered. As the user types in search criteria, a list view of locations is updated to show the relevant locations.
+
+This app uses Knockout, a JavaScript library that helps create rich, responsive display and editor user interfaces with a clean underlying data model. It also uses the Google Maps JavaScript API and the jQuery library.
+
+## Installation
+Make sure you have downloaded app.js, index.html, jquery.min.js, and knockout-3.4.1.js and saved into a common folder in order to use the app.
+
+## How to use the map
+Open up index.html in a browser in order to search for locations.
---
+++ app.js
@@ -0,0 +1,259 @@
+/* I found this codepen that MCS shared on the Udacity discussion forum (https://discussions.udacity.com/t/linking-the-ko-filter-to-the-markers-le-sigh/35771/2) to be very helpful: http://codepen.io/prather-mcs/pen/KpjbNN?editors=0010#0
+*/
+
+// array of EBAC sites
+var locations = [
+    {name: "Administrative Office", address:"303 Van Buren Ave", city: "Oakland", zipcode: "94610", phone:"510-268-3770", latLng: {lat: 37.810924, lng: -122.256157}, services: "Administrative" },
+
+    {name: "Bancroft Counseling Enriched Program", address:"1150 Bancroft Ave.", city: "San Leandro", zipcode: "94577", phone: "510-504-0115", latLng: {lat: 37.727952, lng:  -122.147723}, services: "After-school"},
+
+    {name: "Fremont Healthy Start (Fremont)", latLng: {lat: 37.807709, lng: -122.266436}, lat: 37.807709, lng: -122.266436, services: "Counseling", city: "Oakland"},
+
+    {name: "SBBH Services at Glassbrook Elementary", address: "975 Shafer Road", zipcode: 94544, latLng: {lat: 37.641056, lng: -122.079810}, services: "South Region School Based Behavioral Health Services, Counseling", city: "Hayward"},
+
+    {name: "SBBH Services at Hesperian Elementary", address: "620 Drew Street", zipcode: 94580, latLng: {lat: 37.693460, lng: -122.125025}, services: "South Region School Based Behavioral Health Services",city: "San Lorenzo"},
+
+    {name: "SBBH Services at Hillside Elementary", address: "15980 Marcella Street", zipcode: 94578, latLng: {lat: 37.701747, lng: -122.116367}, services: "South Region School Based Behavioral Health Services, After-school",city: "San Leandro"},
+
+    {name: "SBBH Services at Bowman School", address: "520 Jefferson Street", zipcode: 94544, latLng: {lat: 37.641738, lng: -122.062723}, services: "After-school",city: "Hayward"},
+
+    {name: "SBBH Services at Burbank School", address: "353 B Street", zipcode: 94541, latLng: {lat: 37.668048, lng: -122.094318}, services: "After-school",city: "Hayward"},
+
+    {name: "SBBH Services at Cherryland School", address: "585 Willow Ave", zipcode: 94541, latLng: {lat: 37.675611, lng: -122.098558}, services: "After-school",city: "Hayward"},
+
+    {name: "SBBH Services at Tyrrell School", address: "27000 Tyrrell Ave", zipcode: 94544, latLng: {lat: 37.638178, lng: -122.075084}, services: "After-school",city: "Hayward"},
+
+    {name: "SBBH Services at Martin Luther King, Jr. Middle School", address: "26890 Holly Hill Ave", zipcode: 94545, latLng: {lat: 37.635879, lng: -122.091281}, services: "After-school",city: "Hayward"},
+
+    {name: "SBBH Services at Fremont Durham Elementary", address: "40292 Leslie St.", zipcode: 94545, latLng: {lat: 37.542378, lng: -121.966316}, services: "After-school",city: "Fremont"},
+
+    {name: "Alameda", latLng: {lat: 37.708221, lng: -122.206422}, lat: 37.208221, lng: -122.406422, services: "Counseling",city: "Alameda"},
+
+    {name: "Other", latLng: {lat: 37.708221, lng: -122.206422}, lat: 37.108221, lng: -122.606422, services: "Counseling",city: "San Leandro"}
+];
+
+// customize the styles for the Google map
+var styles = [
+    {
+      elementType: 'geometry',
+      stylers: [{color: '#f5f5f5'}]
+    },
+    {
+      elementType: 'labels.icon',
+      stylers: [{visibility: 'off'}]
+    },
+    {
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#616161'}]
+    },
+    {
+      elementType: 'labels.text.stroke',
+      stylers: [{color: '#f5f5f5'}]
+    },
+    {
+      featureType: 'administrative.land_parcel',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#bdbdbd'}]
+    },
+    {
+      featureType: 'poi',
+      elementType: 'geometry',
+      stylers: [{color: '#eeeeee'}]
+    },
+    {
+      featureType: 'poi',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#757575'}]
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'geometry',
+      stylers: [{color: '#e5e5e5'}]
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#9e9e9e'}]
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: [{color: '#ffffff'}]
+    },
+    {
+      featureType: 'road.arterial',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#757575'}]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry',
+      stylers: [{color: '#dadada'}]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#616161'}]
+    },
+    {
+      featureType: 'road.local',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#9e9e9e'}]
+    },
+    {
+      featureType: 'transit.line',
+      elementType: 'geometry',
+      stylers: [{color: '#e5e5e5'}]
+    },
+    {
+      featureType: 'transit.station',
+      elementType: 'geometry',
+      stylers: [{color: '#eeeeee'}]
+    },
+    {
+      featureType: 'water',
+      elementType: 'geometry',
+      stylers: [{color: '#c9c9c9'}]
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#9e9e9e'}]
+    }
+];
+
+// create variable for infowindows used for markers
+var infowindow = new google.maps.InfoWindow();
+
+// view model for the app
+var ViewModel = function() {
+
+    'use strict';
+
+    var self = this;
+
+    // create the map and store as self.googleMap
+    self.googleMap = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 37.808221, lng: -122.306422},
+        zoom: 11,
+        mapTypeControl: false
+    });
+
+    // set styles for the map
+    self.googleMap.setOptions({styles: styles});
+
+    // error handling if google map doesn't load
+    if (typeof google === 'object' && typeof google.maps === 'object') {
+
+    } else {
+        $('#map-container').html('<p>Unfortunately, the map can not be loaded at this time.</p>')
+    }
+
+    // create array that will store all the locations
+    self.allLocations = [];
+
+    // when first loading the page, iterate through locations array and push instance of Location into allLocations array
+    locations.forEach(function(location) {
+        self.allLocations.push(new Location(location));
+    });
+
+    // build markers for each location in the allLocations array
+    self.allLocations.forEach(function(location) {
+
+        var markerOptions = {
+            map: self.googleMap,
+            position: location.latLng,
+            animation: google.maps.Animation.DROP
+        };
+
+        location.marker = new google.maps.Marker(markerOptions);
+
+        // content loaded into infowindow when marker clicked
+        google.maps.event.addListener(location.marker, 'click', function() {
+
+            infowindow.setContent('<h2>' + location.name + '</h2><p>' + location.address + '<br>' + location.city + ', CA ' + location.zipcode + '</p><p>' + location.services + '</p><h3 class="article-heading">Latest NYTimes article referencing ' + location.city + '</h3><p class="nytimes-article"></p>');
+
+            location.marker.setAnimation(google.maps.Animation.BOUNCE);
+
+            setTimeout(function() { // stop bouncing of marker
+                location.marker.setAnimation(null)
+            }, 1000);
+
+            infowindow.open(self.googleMap, this);
+
+            // call function that displays article referencing location city
+            queryNYTimes(location.city);
+
+        });
+
+        // when user clicks on map outside of marker, close infowindow
+        google.maps.event.addListener(self.googleMap, 'click', function(){
+            infowindow.close();
+        });
+    });
+
+    // create an array for visible markers
+    self.visibleLocations = ko.observableArray();
+
+    // set visibility for all markers
+    self.allLocations.forEach(function(location) {
+        self.visibleLocations.push(location);
+    });
+
+    self.nameSearch = ko.observable();
+
+    // show location markers that match the search input (either by name, city or services)
+    self.filterMarkers = function() {
+        var searchInput = self.nameSearch().toLowerCase();
+
+        self.visibleLocations.removeAll();
+
+        self.allLocations.forEach(function(location) {
+
+            // only want to show markers that meet the criteria
+            location.marker.setVisible(false);
+
+            if ( (location.name.toLowerCase().indexOf(searchInput) !== -1) || (location.services.toLowerCase().indexOf(searchInput) !== -1) || (location.city.toLowerCase().indexOf(searchInput) !== -1)) {
+                self.visibleLocations.push(location); // we store locations that meet critiera into an array that we then iterate through to set visibility to true
+            }
+
+        });
+
+        self.visibleLocations().forEach(function(location) {
+            location.marker.setVisible(true);
+        });
+    };
+
+    // constructor function that creates a prototype, Location. Objects of this type will be created for each location as the array of locations change according to search parameters.  Each object of this type will have the following initialized properties (name, latLng, services, address, city, zipcode, and marker
+    function Location(obj) {
+        this.name = obj.name;
+        this.latLng = obj.latLng;
+        this.services = obj.services;
+        this.address = obj.address;
+        this.city = obj.city;
+        this.zipcode = obj.zipcode;
+        this.marker = null;
+    }
+};
+
+// define function for NYTimes API requests. Pass the city of the selected marker as a query parameter so only articles that reference that city will be retrieved
+function queryNYTimes(city) {
+    var $nytHeader = $('.article-heading');
+    var $nytArticle = $('.nytimes-article');
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + city + '&sort=newest&api-key=42319605f44f49bba54758d38711077f';
+    $.ajax({
+        url: nytimesUrl,
+        dataType: "json",
+        success: function(data) {
+            articles = data.response.docs;
+            var article = articles[0];
+                $nytArticle.html('<h4>' + article.headline.main + '</h4><p>' + article.snippet + '</p>');
+        },
+        error: function(e) {
+            $nytHeader.text('The most recent New York Times article for' + city + ' could not be loaded.');
+        },
+        return :false,
+    });
+}
+
+ko.applyBindings(new ViewModel());
